3181420cc1ce7eebd4d6e173eda1e32867b8fa32
Daniel Ramazani <daniel.ramazani@schwab.com>
2018-01-06 23:01:43 -0600

    added ref to fix tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { ChanComponent } from './chan/chan.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -8,7 +9,8 @@ describe('AppComponent', () => {
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        ChanComponent
       ],
     }).compileComponents();
   }));
